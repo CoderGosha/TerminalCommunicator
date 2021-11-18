34eabd2cc4acf0fb739ff06cc8be13f998975b24
Igor Pakholkov <pi@en-gy.ru>
2021-11-18 11:34:19 +0300

    test 53

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python3 manage.py migrate Communicator
 # python3 manage.py runserver 0.0.0.0:8000
-gunicorn -b 0.0.0.0:8000 --workers=2 --threads=4 --worker-class=gthread Comminucator.wsgi:application
+gunicorn -b 0.0.0.0:8000 --workers=2 --threads=4 --worker-class=gthread Communicator.wsgi:application
