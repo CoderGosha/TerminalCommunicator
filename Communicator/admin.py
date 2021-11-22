70f41a1bac5dbf5f309b215f34b6d45d67feff80
Igor Pakholkov <pi@en-gy.ru>
2021-11-22 21:55:45 +0300

    fix django admin

--- Communicator/admin.py
+++ Communicator/admin.py
@@ -6,7 +6,30 @@ from django.contrib.auth.models import Permission
 from Communicator.models import Event
 from Communicator.models.terminal import Terminal
 
-admin.site.register(Terminal)
-admin.site.register(Event)
+from django.contrib import admin
+
+
+class EventAdmin(admin.ModelAdmin):
+    model = Event
+    list_per_page = 10  # No of records per page
+    list_display = ('event_type', 'terminal', 'data_create', 'success')
+    list_filter = ('terminal',)
+    ordering = ('-data_create',)
+    search_fields = ('request',)
+    pass
+
+
+class TerminalAdmin(admin.ModelAdmin):
+    model = Terminal
+    list_per_page = 10  # No of records per page
+    list_display = ('name', 'owner', 'is_active', 'last_connect', 'ip_address', 'count_request' )
+    list_filter = ('owner',)
+    ordering = ('-last_connect',)
+    search_fields = ('name',)
+    pass
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Terminal, TerminalAdmin)
 admin.site.register(Permission)
 
