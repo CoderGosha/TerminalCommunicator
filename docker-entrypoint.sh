088f1fea0e06485b30d304c4c6aeebc60ef11fe7
Igor Pakholkov <pi@en-gy.ru>
2021-11-28 23:18:44 +0300

    test 151

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,6 +14,7 @@ fi
 python manage.py flush --no-input
 python manage.py migrate
 
+python manage.py collectstatic --no-input
 # python3 manage.py migrate Communicator
 # python3 manage.py runserver 0.0.0.0:8000
 exec "$@"
